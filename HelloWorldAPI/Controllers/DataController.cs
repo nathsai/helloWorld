177ec56befddde80e61e9855874f69f28a1ae5c0
nathsai <Dimple.sai143>
2019-07-08 10:46:30 -0500

    Project commit for IN

---
+++ HelloWorldAPI/Controllers/DataController.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace HelloWorldAPI.Controllers
+{
+    using System.Configuration;
+    using System.IO;
+    using System.Net;
+    using System.Web.Http;
+    using HelloWorldInfrastructure.Attributes;
+    using HelloWorldInfrastructure.Models;
+    using HelloWorldInfrastructure.Services;
+
+    /// <summary>
+    ///     API controller for getting and setting today's value.
+    /// </summary>
+    [WebApiExceptionFilter]
+    public class DataController : ApiController
+    {
+        /// <summary>
+        ///     The data service
+        /// </summary>
+        private readonly IDataService dataService;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="DataController" /> class.
+        /// </summary>
+        /// <param name="dataService">The injected data service</param>
+        public DataController(IDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+
+        /// <summary>
+        ///     Gets today's value
+        /// </summary>
+        /// <returns>A TodaysData model containing today's value</returns>
+        [WebApiExceptionFilter(Type = typeof(IOException), Status = HttpStatusCode.ServiceUnavailable, Severity = SeverityCode.Error)]
+        [WebApiExceptionFilter(Type = typeof(SettingsPropertyNotFoundException), Status = HttpStatusCode.ServiceUnavailable, Severity = SeverityCode.Error)]
+        public Data Get()
+        {
+            return this.dataService.GetTodaysData();
+        }
+    }
+}
